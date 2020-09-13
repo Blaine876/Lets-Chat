d138116507c36db3d25b5558fa8b2f445c64cfff
Blaine Oakleym <blaine.oakley@gmailcom>
2020-09-12 23:22:47 -0400

    Added chat footer

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,5 +1,11 @@
 import { Avatar, IconButton } from "@material-ui/core";
-import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import {
+  AttachFile,
+  InsertEmoticon,
+  Mic,
+  MoreVert,
+  SearchOutlined,
+} from "@material-ui/icons";
 import React from "react";
 import "./Chat.css";
 interface Props {}
@@ -44,6 +50,15 @@ const Chat = (props: Props) => {
           This is a message
           <span className="chat__timestamp">{new Date().toUTCString()}</span>
         </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input type="text" placeholder="Type a message" />
+          <button type="submit">Send a message</button>
+        </form>
+        <Mic />
       </div>
     </div>
   );
