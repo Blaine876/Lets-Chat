c4b73c818e0c265c6d3a3b8683fd2053b92d4870
Blaine Oakleym <blaine.oakley@gmailcom>
2020-09-13 18:44:38 -0400

    Added Pusher for realtime triggers on messages

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const Pusher = require("pusher");
 const messageRoute = require("./routes/message");
 
 //APP CONFIG
@@ -11,7 +12,24 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 //MIDDLWARE
+const pusher = new Pusher({
+  appId: "1072291",
+  key: "835ec4ab25e31535c4a2",
+  secret: "c1ce7e478d7c6a3ed336",
+  cluster: "us2",
+  useTLS: true,
+});
+
+app.use(morgan("common"));
+app.use(helmet());
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
 
 //DB CONNECT
 const db_URI =
@@ -23,6 +41,28 @@ mongoose.connect(db_URI, {
   useUnifiedTopology: true,
 });
 // ????
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("DB Connected");
+  const msgCollection = db.collection("messages");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log("A change occured", change);
+
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.name,
+        message: messageDetails.message,
+      });
+    } else {
+      console.log("Error triggering Pusher");
+    }
+  });
+});
 
 //API ROUTES
 app.get("/", (req, res) => {
